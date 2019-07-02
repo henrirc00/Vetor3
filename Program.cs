ab8c99a5555e63c12a0d722b6e0bff047a16e1f9
HENRIQUE RODRIGUES DA CUNHA <henrique.rcunha1@senacsp.edu.br>
2019-07-02 11:06:49 -0300

    Realizando a soma dos  Valores do Vetor

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Vetor3
     {
         static void Main(string[] args)
         {
-        
+        int [] numeros={10, 23, 5, 45, 68, 12, 70};
+        Console.WriteLine("Qtd do Vetor. "+numeros.Length);
+        int rs = numeros[0];
+        for(int i = 1; i < numeros.Length; i++){
+            rs+=numeros[i];
+
         }
+            Console.WriteLine(rs);
+
+        }
+
+        
     }
 }
